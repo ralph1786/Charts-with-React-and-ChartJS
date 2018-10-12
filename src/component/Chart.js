0f0b09a7a68d6adc9c509b9118646d5431dc949e
Rafael Cruz <rafac@Rafaels-MacBook-Pro.local>
2018-10-12 11:37:46 -0400

    updated package.json

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -1,98 +1,104 @@
-import React, { Component } from 'react'
-import { Bar, Line, Pie, Doughnut } from 'react-chartjs-2';
+import React, { Component } from "react";
+import { Bar, Line, Pie, Doughnut } from "react-chartjs-2";
 
-export default class Chart extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            chartData: props.chartData
-        }
-    }
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: props.chartData
+    };
+  }
 
-    static defaultProps = {
-        displayTitle: true,
-        displayLegend: true,
-        legendPosition: 'right',
-        location: "City",
-        displayFont: "Impact"
-    }
-  
-    render() {
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: "right",
+    location: "City",
+    displayFont: "Impact"
+  };
+
+  render() {
     return (
-      <div className='chart'>
+      <div className="chart">
         <Bar
-            data={this.state.chartData}
-            options={{
-                title: {
-                    display: this.props.displayTitle,
-                    text: `Population in ${this.props.location}`,
-                    fontSize: 24,
-                    fontFamily: this.props.displayFont
-                },
-                legend: {
-                    display: this.props.displayLegend,
-                    position: this.props.legendPosition
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                        }
-                    }]
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: `Population in ${this.props.location}`,
+              fontSize: 24,
+              fontFamily: this.props.displayFont
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
                 }
-            }}
+              ]
+            }
+          }}
         />
         <Line
-            data={this.state.chartData}
-            options={{
-                title: {
-                    display: this.props.displayTitle,
-                    text: `Population in ${this.props.location}`,
-                    fontSize: 24
-                },
-                legend: {
-                    display: this.props.displayLegend,
-                    position: this.props.legendPosition
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                        }
-                    }]
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: `Population in ${this.props.location}`,
+              fontSize: 24
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
                 }
-            }}
+              ]
+            }
+          }}
         />
         <Pie
-            data={this.state.chartData}
-            options={{
-                title: {
-                    display: this.props.displayTitle,
-                    text: `Population in ${this.props.location}`,
-                    fontSize: 24,
-                    fontFamily: this.props.displayFont
-                },
-                legend: {
-                    display: this.props.displayLegend,
-                    position: 'right'
-                }
-            }}
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: `Population in ${this.props.location}`,
+              fontSize: 24,
+              fontFamily: this.props.displayFont
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: "right"
+            }
+          }}
         />
         <Doughnut
-            data={this.state.chartData}
-            options={{
-                title: {
-                    display: this.props.displayTitle,
-                    text: `Population in ${this.props.location}`,
-                    fontSize: 24
-                },
-                legend: {
-                    display: this.props.displayLegend,
-                    position: this.props.legendPosition
-                }
-            }}
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: `Population in ${this.props.location}`,
+              fontSize: 24
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
         />
       </div>
-    )
+    );
   }
 }
+
+export default Chart;
